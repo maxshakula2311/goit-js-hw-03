9cc708a7c2de2e39d3c76793b39d2ad82df16f6c
Max Shakula <maxshakulakr@gmail.com>
2025-11-09 22:55:08 +0200

    done

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,36 @@
-function makeTransaction(quantity, pricePerDroid) {
-    let totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;   
+function slugify(title) {
+    const lowerCase = title.toLowerCase();
+    const arrSplit = lowerCase.split(' ');
+    const slug = arrSplit.join('-');
+    return slug;
 }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+console.log('-------------------------------------'); //divider
+
+
+
+// I tried to combine this function with adding a link base.
+
+
+
+
+// function slugifyLink(title) {
+//   const lowerCase = title.toLowerCase();
+//   const arrSplit = lowerCase.split(' ');
+//     const slug = arrSplit.join('-');
+//     const linkBase = 'https://neobound.com/';
+//     const fullLink = linkBase + slug;
+//     return fullLink;
+// }
+
+// console.log(slugifyLink("Arrays for beginners")); // "arrays-for-beginners"
+// console.log(slugifyLink("English for developer")); // "english-for-developer"
+// console.log(slugifyLink("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+// console.log(slugifyLink("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+// console.log('-------------------------------------'); //divider
